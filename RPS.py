053134837b5dd596da62e79149c12624eb1b962e
Dede Pariu <nitroshard@gmail.com>
2025-03-29 05:27:03 +0000

    quin50 abbe0.2 kris99 mrug99

--- RPS.py
+++ RPS.py
@@ -1,10 +1,21 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+def player(prev_play, opponent_history=[], my_history=[]):
+    if prev_play == "":
+        my_history.append('R')
+        return 'R'
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    response = {'R':'S', 'P':'R', 'S':'P'}
+    next_play = response[my_history[-1]]
+    my_history.append(next_play)
+    return next_play
 
-    return guess
+    # if my_history[-1] == 'R':
+    #     my_history.append('S')
+    #     return 'S'
+    # elif my_history[-1] == 'P':
+    #     my_history.append('R')
+    #     return 'R'
+    # elif my_history[-1] == 'S':
+    #     my_history.append('P')
+    #     return 'P'
